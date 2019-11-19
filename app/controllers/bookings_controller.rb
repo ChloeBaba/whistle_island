d884460f7dbbdcfb1054bd7a68dcbd32012d8551
Dieter Pankratz <pankratz117@gmail.com>
2019-11-19 17:52:23 +0100

    add new action to bookings controller AND fixed a syntax error in booking_policy.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,7 @@
 class BookingsController < ApplicationController
+  def new
+    @island = Island.find(params[:island_id])
+    @booking = Booking.new
+    authorize @booking
+  end
 end
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -19,7 +19,7 @@ class ApplicationPolicy
   end
 
   def new?
-    create?
+    false
   end
 
   def update?
