5ac6de6e18cede37274484aaa482c849faa2fa22
Lyndsey Reed <ler2144@tc.columbia.edu>
2019-11-19 12:23:06 +0100

    island controller

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,2 +1,17 @@
 class IslandsController < ApplicationController
+  def create
+  def create
+    @island = Island.new(island_params)
+    if @island.save
+      redirect_to islands_path
+    else
+      render :new
+    end
+  end
 end
+
+
+private
+
+def is_owner?
+  record.user == user || user.admin
