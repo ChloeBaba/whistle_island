3461965abf39bf9ada6b776e83f4ca9b3a0a1db1
Thomas Lacheze <thomas.lchz@gmail.com>
2019-11-19 11:52:42 +0100

    added routes and edit/update

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,2 +1,21 @@
 class IslandsController < ApplicationController
+  def edit
+    set_island
+  end
+
+  def update
+    set_island
+    @island.update(island_params)
+    redirect_to island_path(@island)
+  end
+
+private
+
+  def set_island
+    @island = Island.find(params[:id])
+  end
+
+  def island_params
+    params.require(:island).permit(:name, :description, :availability, :price, :photo)
+  end
 end
