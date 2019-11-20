1c146d96321208c105e97c99b53f0fa133c81c88
chloe barreau <cbarreau9@gmail.com>
2019-11-20 21:17:51 +0100

    created users controller and empty show, edit and update methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  get 'users/:id', to: 'users#show', as: 'profile'
+  get 'users/:id', to: 'users#show'
   get 'users/:id/edit', to: 'users#edit'
   patch 'users/:id', to: 'users#update'
   put 'users/:id', to: 'users#update'
