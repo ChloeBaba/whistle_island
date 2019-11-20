37fefc9de157cd53e64c6592edcb2d2b42a4a95d
Dieter Pankratz <pankratz117@gmail.com>
2019-11-20 23:47:49 +0100

    add-profile-view-for-current-user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   get 'users/:id', to: 'users#show'
-  get 'users/:id/edit', to: 'users#edit'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
   patch 'users/:id', to: 'users#update'
   put 'users/:id', to: 'users#update'
   get 'dashboard', to: 'dashboards#show', as: 'dashboard'
