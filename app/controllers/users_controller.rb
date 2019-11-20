2f52cf78b55fdc5318d359965291acac2c323c26
Dieter Pankratz <pankratz117@gmail.com>
2019-11-21 00:32:37 +0100

    add edit action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+    authorize @user
   end
 
   def update
+    current_user.update
+    redirect_to dashboard_path
+    authorize current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:current_user).permit(:name, :email, :profile_photo)
   end
 end
