255f897e31e2c8a92d2f4f2f417066ba2d93ca16
Thomas Lacheze <thomas.lchz@gmail.com>
2019-11-21 11:14:12 +0100

    retrieving user pull

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2019_11_20_191640) do
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.bigint "island_id"
+    t.string "photo_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["island_id"], name: "index_photos_on_island_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +65,5 @@ ActiveRecord::Schema.define(version: 2019_11_20_191640) do
   add_foreign_key "bookings", "islands"
   add_foreign_key "bookings", "users"
   add_foreign_key "islands", "users"
+  add_foreign_key "photos", "islands"
 end
