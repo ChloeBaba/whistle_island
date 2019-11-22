dc22b7656df2b5f84284f9d639fa1e8970cbc1fe
Lyndsey Reed <ler2144@tc.columbia.edu>
2019-11-22 11:16:31 +0100

    slogan update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_153730) do
+ActiveRecord::Schema.define(version: 2019_11_22_100144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_153730) do
     t.float "latitude"
     t.float "longitude"
     t.string "location"
+    t.string "slogan"
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
