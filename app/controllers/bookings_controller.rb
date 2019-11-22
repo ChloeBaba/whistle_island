e76030b0fa43fdc118b7f0e519eb4a90b02e7aaa
Dieter Pankratz <pankratz117@gmail.com>
2019-11-22 10:43:42 +0100

    fixed typo onwer -> owner

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,7 +10,7 @@ class BookingsController < ApplicationController
 
   def new
     @island = Island.find(params[:island_id])
-    @island_ownewr = @island.user
+    @island_owner = @island.user
     @booking = Booking.new
     authorize @booking
   end
