8e2dc63ec44db47605a08c56eee7186897669bff
chloe barreau <cbarreau9@gmail.com>
2020-04-06 10:17:18 +0200

    Upgrade kind-of to version 6.0.3 in yarn.lock file as the previous version was 6.0.2. It was triggering security alert in git

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_100144) do
+ActiveRecord::Schema.define(version: 2019_11_22_104801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 2019_11_22_100144) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
     t.string "location"
     t.string "slogan"
+    t.string "photo"
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
